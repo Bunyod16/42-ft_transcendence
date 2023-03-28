d389d5b8fe4c66dd61e5f56972393bf737877cf1
Bunyod16 <bunyodshams@gmail.com>
2023-03-28 23:15:57 +0800

    chore: add database to global scope

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TwoFactorModule } from './two_factor/two_factor.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     UserModule,
+    TwoFactorModule,
+    DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from '../database/database.module';
 import { UserController } from './user.controller';
 import { userProviders } from './user.providers';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule],
+  imports: [ConfigModule],
   providers: [
     ...userProviders,
     UserService,
