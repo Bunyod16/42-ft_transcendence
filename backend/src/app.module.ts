33dc7f5523e30092fffbbd9f7634b1446176aa76
Bunyod16 <bunyodshams@gmail.com>
2023-03-28 18:38:04 +0800

    chore: cleanup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { TwoFactorModule } from './two_factor/two_factor.module';
 
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    TwoFactorModule,
-    // MatchesModule,
   ],
   controllers: [
     AppController,
