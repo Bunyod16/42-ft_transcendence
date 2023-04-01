3f1d8bd93b7900a85a4051330126572c4fda99c0
Jason <jasonkwm13@gmail.com>
2023-04-01 11:59:19 +0800

    chores: commit code before testing out eslint

---
+++ frontend/src/components/LogInBox.tsx
@@ -0,0 +1,15 @@
+import { Box, Button } from '@mui/material';
+
+export default function LoginPanel() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+padding: '40px 20px',
+      }}
+    />
+  );
+}
--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,5 +1,16 @@
+import { Box, Button, Container, Typography } from "@mui/material";
+
 export default function Login() {
-	return (
-		<></>
-	)
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        background: "#AAAAAA",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    />
+  );
 }
