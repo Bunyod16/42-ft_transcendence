92a300644c9d888dd99db07b8606938512983e90
Bunyod16 <bunyodshams@gmail.com>
2023-04-11 22:21:39 +0800

    chore: end game on user disconnect

--- backend/src/game_stream/game_stream.module.ts
+++ backend/src/game_stream/game_stream.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { GameStreamGateway } from './game_stream.gateway';
 import { GameStateModule } from 'src/game_state/gameState.module';
 import { GameStreamService } from './game_stream.service';
+import { MatchModule } from 'src/match/match.module';
+import { JwtAccessModule } from 'src/jwt_access/jwt_access.module';
 
 @Module({
-  imports: [GameStateModule],
+  imports: [GameStateModule, JwtAccessModule, MatchModule],
   providers: [GameStreamGateway, GameStreamService],
-  exports: [GameStreamGateway, GameStreamService]
+  exports: [GameStreamGateway, GameStreamService],
 })
 export class GameStreamModule {}
--- backend/src/match/match.module.ts
+++ backend/src/match/match.module.ts
@@ -2,23 +2,14 @@ import { Module } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
 import { ConfigModule } from '@nestjs/config';
-import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Match } from './entities/match.entity';
-import { UserModule } from 'src/user/user.module';
 import { GameStateModule } from 'src/game_state/gameState.module';
-import { GameStreamModule } from 'src/game_stream/game_stream.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Match]),
-    ConfigModule,
-    UserModule,
-    GameStateModule,
-    GameStreamModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Match]), ConfigModule, GameStateModule],
   controllers: [MatchController],
-  providers: [MatchService, UserService],
+  providers: [MatchService],
   exports: [MatchService, TypeOrmModule],
 })
 export class MatchModule {}
