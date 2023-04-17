b766a825406ae5b0188785c10d931b846b9f6fee
Wang Khang Waei <devtesting404@gmail.com>
2023-04-17 20:55:08 +0800

    Add content module and controller for image upload

---
+++ backend/src/content/content.controller.ts
@@ -0,0 +1,52 @@
+import { Controller, Post, UploadedFile, ParseFilePipe, FileTypeValidator, MaxFileSizeValidator, Logger, HttpStatus, Body, UseGuards, Req, UseInterceptors } from '@nestjs/common';
+import { UserAuthGuard } from 'src/auth/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { UserService } from 'src/user/user.service';
+import RequestWithUser from 'src/auth/requestWithUser.interace';
+
+@Controller('content')
+export class ContentController {
+  private readonly logger = new Logger(ContentController.name);
+
+  constructor(private userService: UserService){}
+
+  @UseGuards(UserAuthGuard)
+	@Post('upload_avatar')
+  @UseInterceptors(
+    FileInterceptor('file',
+    {
+      storage: diskStorage({
+        destination: '../../avatars/',
+        filename: (req: RequestWithUser, file: Express.Multer.File, callback) => {
+          const dateObj = new Date();
+          const dateSuffix = dateObj.getFullYear() + dateObj.getMonth() + dateObj.getDay();
+          callback(null, dateSuffix + '-' + req.user.nickName + '.' + file.originalname.split('.').pop());
+        }
+      })
+    }
+    )
+  )
+	uploadFile(
+    @Req() req: RequestWithUser,
+    @Body() body: Request,
+    @UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new FileTypeValidator({ fileType: 'image/png' }),
+        new MaxFileSizeValidator({ maxSize: 2048 })
+      ],
+      errorHttpStatusCode: HttpStatus.BAD_REQUEST
+    })
+  ) file: Express.Multer.File): string {
+    this.logger.log(`Saving avatar for user ${req.user.nickName} to ${file.destination}`);
+    const dateObj = new Date();
+    const dateSuffix = dateObj.getFullYear() + dateObj.getMonth() + dateObj.getDay();
+    const filename = dateSuffix + '-' + req.user.nickName + '.' + file.originalname.split('.').pop();
+
+    this.userService.update(req.user.id, {
+      avatar: filename
+    });
+    return "File successfully uploaded";
+	}
+}
---
+++ backend/src/content/content.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { ContentController } from './content.controller';
+import { diskStorage } from 'multer';
+import { UserModule } from 'src/user/user.module';
+import RequestWithUser from 'src/auth/requestWithUser.interace';
+
+@Module({
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: '../../avatars/',
+        filename: (req, file: Express.Multer.File, callback) => {
+          const dateObj = new Date();
+          const dateSuffix = dateObj.getFullYear() + dateObj.getMonth() + dateObj.getDay();
+          callback(null, dateSuffix + '-' + file.fieldname + '.' + file.originalname.split('.').pop());
+        }
+      })
+    }),
+    UserModule
+  ],
+  controllers: [ContentController]
+})
+export class ContentModule {}
