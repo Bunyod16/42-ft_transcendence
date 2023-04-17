fb72d4600eb195f98b4c03aa166e61205a1165dd
Wang Khang Waei <devtesting404@gmail.com>
2023-04-17 15:29:24 +0800

    delete src/chat/chat.controller.ts

--- backend/src/chat/chat.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Body, Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { UserAuthGuard } from 'src/auth/auth.guard';
-
-@UseGuards(UserAuthGuard)
-@Controller('chat')
-export class ChatController {
-}
