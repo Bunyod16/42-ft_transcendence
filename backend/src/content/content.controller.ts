6ed63022d0a35e36518fca8dfe00d655eaa9ece1
Wang Khang Waei <devtesting404@gmail.com>
2023-04-18 15:05:17 +0800

    Fix destination path

--- backend/src/content/content.controller.ts
+++ backend/src/content/content.controller.ts
@@ -4,6 +4,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { UserService } from 'src/user/user.service';
 import RequestWithUser from 'src/auth/requestWithUser.interace';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('content')
 export class ContentController {
@@ -14,37 +15,41 @@ export class ContentController {
   @UseGuards(UserAuthGuard)
 	@Post('upload_avatar')
   @UseInterceptors(
-    FileInterceptor('file',
+    FileInterceptor('avatar',
     {
       storage: diskStorage({
-        destination: '../../avatars/',
+        destination: 'avatars/',
         filename: (req: RequestWithUser, file: Express.Multer.File, callback) => {
-          const dateObj = new Date();
-          const dateSuffix = dateObj.getFullYear() + dateObj.getMonth() + dateObj.getDay();
-          callback(null, dateSuffix + '-' + req.user.nickName + '.' + file.originalname.split('.').pop());
+          callback(null, req.user.id + '-' + req.user.nickName + '.' + file.originalname.split('.').pop());
         }
       })
     }
     )
   )
-	async uploadFile(
+	async uploadAvatar(
     @Req() req: RequestWithUser,
     @Body() body: Request,
     @UploadedFile(
     new ParseFilePipe({
       validators: [
         new FileTypeValidator({ fileType: 'image/png' }),
-        new MaxFileSizeValidator({ maxSize: 2048 })
+        new MaxFileSizeValidator({ maxSize: 2000000 })
       ],
       errorHttpStatusCode: HttpStatus.BAD_REQUEST
     })
   ) file: Express.Multer.File) {
-    this.logger.log(`Saving avatar for user ${req.user.nickName} to ${file.destination}`);
-    const dateObj = new Date();
-    const dateSuffix = dateObj.getFullYear() + dateObj.getMonth() + dateObj.getDay();
-    const filename = dateSuffix + '-' + req.user.nickName + '.' + file.originalname.split('.').pop();
+    const { user } = req;
 
-    await this.userService.update(req.user.id, {
+    const filename = user.id + '-' + user.nickName + '.' + file.originalname.split('.').pop();
+    this.logger.log(`Saving avatar for user ${user.nickName} to ${file.destination} as ${filename}`);
+
+    const retUser: User = await this.userService.findOne(user.id);
+
+    // if (retUser.avatar !== 'default-stormtrooper.png') {
+      // send request to delete user.avatar on cdn.
+    // }
+
+    await this.userService.update(user.id, {
       avatar: filename
     });
 
