d35052a18f6173519c8748a3ec43f63f20e0ec1c
Wang Khang Waei <devtesting404@gmail.com>
2023-04-26 13:33:57 +0800

    Remove trailing whitespace from create-match.dto.ts

--- backend/src/match/dto/create-match.dto.ts
+++ backend/src/match/dto/create-match.dto.ts
@@ -3,7 +3,7 @@ import { User } from 'src/user/entities/user.entity';
 // import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateMatchDto {
-  
+
   // @ApiProperty
   @Allow()
   playerOne: User;
