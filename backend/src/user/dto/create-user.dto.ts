b910ff100702a78e18f4728e652725c96c5a8e7c
Wang Khang Waei <devtesting404@gmail.com>
2023-04-26 13:36:54 +0800

    Add validation to create-user.dto.ts

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,31 +1,45 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Allow, IsArray, IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
 import { Match } from 'src/match/entities/match.entity';
 import { UserAchievement } from 'src/user_achievement/entities/user_achievement.entity';
 
 export class CreateUserDto {
+
   @ApiProperty()
+  @IsString()
   intraName: string;
 
+  @IsString()
   nickName: string;
 
+  @IsDate()
   createdAt: Date;
 
+  @IsDate()
   updatedAt: Date;
 
   @ApiProperty()
+  @IsString()
   password: string;
 
+  @IsNumber()
   wins: number;
 
+  @IsString()
   avatar: string;
 
+  @IsNumber()
   losses: number;
 
+  @IsArray()
   achievements: UserAchievement[];
 
+  @IsArray()
   matchesAsPlayerOne: Match[];
 
+  @IsArray()
   matchesAsPlayerTwo: Match[];
 
+  @IsBoolean()
   online: boolean;
 }
