655bf7d064000e2c691b24cea379821f8c362de4
Wang Khang Waei <devtesting404@gmail.com>
2023-04-26 14:16:52 +0800

    Refactor auth.guard.ts syntax for websockets

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -5,17 +5,34 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Request } from 'express';
-import { JwtRefreshService } from 'src/jwt_refresh/jwt_refresh.service';
 import { parse } from 'cookie';
 import { JwtAccessService } from 'src/jwt_access/jwt_access.service';
+import { Socket } from 'socket.io';
+import { SocketWithAuthData } from 'src/socket_io_adapter/socket-io-adapter.types';
+
+interface Token {
+  refresh: string,
+  access: string
+}
 
 @Injectable()
 export class UserAuthGuard implements CanActivate {
   constructor(private readonly jwtAccessService: JwtAccessService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request, context);
+
+    let cookie: string;
+
+    if (context.getType() === 'http'){
+      const request: Request = context.switchToHttp().getRequest();
+      cookie = request.headers.cookie;
+    }
+    else if (context.getType() === 'ws'){
+      const socket: Socket = context.switchToWs().getClient();
+      cookie = socket.handshake.headers.cookie;
+    }
+
+    const token: Token | null = this.extractTokenFromCookie(cookie);
     if (!token) {
       console.log('Unauthorized request');
       throw new UnauthorizedException();
@@ -24,7 +41,18 @@ export class UserAuthGuard implements CanActivate {
       const payload = await this.jwtAccessService.verifyAccessToken(
         token.access,
       );
-      request['user'] = payload;
+
+      // Not sure if guards should have greater than 1 responsibility, perhaps appending user can be done in an interceptor.
+      // Feels like redundant code as a result..
+      if (context.getType() === 'http'){
+        let request: Request = context.switchToHttp().getRequest();
+        request['user'] = payload;
+      }
+      else if (context.getType() === 'ws'){
+        let socket: SocketWithAuthData = context.switchToWs().getClient();
+        socket.user = payload;
+      }
+
       return true;
     } catch {
       console.log('AccessToken has expired');
@@ -32,30 +60,20 @@ export class UserAuthGuard implements CanActivate {
     }
   }
 
-  extractTokenFromHeader(
-    request: Request,
-    context: any,
-  ): { refresh: string; access: string } | undefined {
+  extractTokenFromCookie(
+    cookie: string,
+  ): Token | null {
     try {
-      var cookieHeader = request.headers.cookie;
-      const cookies = parse(cookieHeader);
-      const refresh = cookies.Refresh;
-      const access = cookies.Authentication;
-      if (refresh && access) return { refresh, access };
+      const cookies = parse(cookie);
+      const refresh: string = cookies.Refresh;
+      const access: string = cookies.Authentication;
+      if (refresh && access){
+        return { refresh, access };
+      }
     } catch {
-      console.log('Request did not have http cookie');
+      console.log('No JWT was found in cookie');
     }
 
-    try {
-      cookieHeader = context.getArgs()[0].handshake.headers.cookie;
-      const cookies = parse(cookieHeader);
-      const refresh = cookies.Refresh;
-      const access = cookies.Authentication;
-      if (refresh && access) return { refresh, access };
-    } catch {
-      console.log('Request did not have websocket cookie');
-    }
-
-    return undefined;
+    return null;
   }
 }
