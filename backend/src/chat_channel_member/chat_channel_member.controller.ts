42b910b97ceee7f25fab743d5dbd35cb1f88823f
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2023-05-04 02:07:43 +0800

    forgot: Forgot to commit return for unmute controller

--- backend/src/chat_channel_member/chat_channel_member.controller.ts
+++ backend/src/chat_channel_member/chat_channel_member.controller.ts
@@ -429,7 +429,7 @@ export class ChatChannelMemberController {
       `ChatChannelMember => update_unmute()`,
     );
 
-    // return chatChannelMember;
+    return chatChannelMember;
   }
 
   @Delete('/byUserInChatChannel')
